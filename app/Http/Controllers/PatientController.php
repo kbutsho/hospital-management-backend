e1b944e068ecc95da335514d731fca3426aaaf88
Kaushik Biswas <kbutsho@gmail.com>
2024-01-02 23:31:32 +0600

    added find patient by phone

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -14,4 +14,24 @@ use App\Validations\PatientValidation;
 
 class PatientController extends Controller
 {
+    public function getPatientByPhone($phone)
+    {
+        try {
+            $patient = Patient::where('phone', $phone)->first();
+            if ($patient) {
+                return response()->json([
+                    'status' => true,
+                    'message' => 'patient found in this number!',
+                    'data' => $patient
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'patient not found!'
+                ], 201);
+            }
+        } catch (\Exception $e) {
+            return ExceptionHandler::handleException($e);
+        }
+    }
 }
