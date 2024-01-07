ae882c0d6cf99e4112b9437107fbaae01ae74325
Kaushik Biswas <kbutsho@gmail.com>
2024-01-07 21:40:46 +0600

    added appointment seeder

---
+++ database/seeders/AppointmentSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Appointment;
+use Illuminate\Database\Seeder;
+
+class AppointmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $appointments = [
+            ['serial_id' => 1, 'serial_number' => 1, 'created_at' => now(), 'updated_at' => now()]
+        ];
+        Appointment::insert($appointments);
+    }
+}
