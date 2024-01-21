a0c693133bad9e3cc2a9febffa07b1de3afd8250
Kaushik Biswas <kbutsho@gmail.com>
2024-01-21 22:05:49 +0600

    added doctor fees seeder

--- database/seeders/DoctorFeeSeeder.php
+++ database/seeders/DoctorFeeSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\DoctorFee;
+use App\Models\DoctorsFee;
 use Illuminate\Database\Seeder;
 
 class DoctorFeeSeeder extends Seeder
@@ -13,22 +13,22 @@ class DoctorFeeSeeder extends Seeder
     public function run(): void
     {
         $fees = [
-            ['doctor_id' => 1, 'fee' => 1000, 'created_at' => now(), 'updated_at' => now()],
-            ['doctor_id' => 2, 'fee' => 1000, 'created_at' => now(), 'updated_at' => now()],
-            ['doctor_id' => 3, 'fee' => 1000, 'created_at' => now(), 'updated_at' => now()],
-            ['doctor_id' => 4, 'fee' => 1000, 'created_at' => now(), 'updated_at' => now()],
-            ['doctor_id' => 5, 'fee' => 1000, 'created_at' => now(), 'updated_at' => now()],
-            ['doctor_id' => 6, 'fee' => 1000, 'created_at' => now(), 'updated_at' => now()],
-            ['doctor_id' => 7, 'fee' => 1000, 'created_at' => now(), 'updated_at' => now()],
-            ['doctor_id' => 8, 'fee' => 1000, 'created_at' => now(), 'updated_at' => now()],
-            ['doctor_id' => 9, 'fee' => 1000, 'created_at' => now(), 'updated_at' => now()],
-            ['doctor_id' => 10, 'fee' => 1000, 'created_at' => now(), 'updated_at' => now()],
-            ['doctor_id' => 11, 'fee' => 1000, 'created_at' => now(), 'updated_at' => now()],
-            ['doctor_id' => 12, 'fee' => 1000, 'created_at' => now(), 'updated_at' => now()],
-            ['doctor_id' => 13, 'fee' => 1000, 'created_at' => now(), 'updated_at' => now()],
-            ['doctor_id' => 14, 'fee' => 1000, 'created_at' => now(), 'updated_at' => now()],
-            ['doctor_id' => 15, 'fee' => 1000, 'created_at' => now(), 'updated_at' => now()],
+            ['doctor_id' => 1, 'fees' => 1000, 'created_at' => now(), 'updated_at' => now()],
+            ['doctor_id' => 2, 'fees' => 1000, 'created_at' => now(), 'updated_at' => now()],
+            ['doctor_id' => 3, 'fees' => 1000, 'created_at' => now(), 'updated_at' => now()],
+            ['doctor_id' => 4, 'fees' => 1000, 'created_at' => now(), 'updated_at' => now()],
+            ['doctor_id' => 5, 'fees' => 1000, 'created_at' => now(), 'updated_at' => now()],
+            ['doctor_id' => 6, 'fees' => 1000, 'created_at' => now(), 'updated_at' => now()],
+            ['doctor_id' => 7, 'fees' => 1000, 'created_at' => now(), 'updated_at' => now()],
+            ['doctor_id' => 8, 'fees' => 1000, 'created_at' => now(), 'updated_at' => now()],
+            ['doctor_id' => 9, 'fees' => 1000, 'created_at' => now(), 'updated_at' => now()],
+            ['doctor_id' => 10, 'fees' => 1000, 'created_at' => now(), 'updated_at' => now()],
+            ['doctor_id' => 11, 'fees' => 1000, 'created_at' => now(), 'updated_at' => now()],
+            ['doctor_id' => 12, 'fees' => 1000, 'created_at' => now(), 'updated_at' => now()],
+            ['doctor_id' => 13, 'fees' => 1000, 'created_at' => now(), 'updated_at' => now()],
+            ['doctor_id' => 14, 'fees' => 1000, 'created_at' => now(), 'updated_at' => now()],
+            ['doctor_id' => 15, 'fees' => 1000, 'created_at' => now(), 'updated_at' => now()],
         ];
-        DoctorFee::insert($fees);
+        DoctorsFee::insert($fees);
     }
 }
