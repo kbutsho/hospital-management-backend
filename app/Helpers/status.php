ff0db50900f387bdac5d847495480ca4585b5673
Kaushik Biswas <kbutsho@gmail.com>
2024-01-23 21:59:21 +0600

    added appointment status

--- app/Helpers/status.php
+++ app/Helpers/status.php
@@ -7,4 +7,7 @@ class STATUS
     const ACTIVE = 'active';
     const PENDING = 'pending';
     const DISABLE = 'disable';
+    const CONFIRMED = 'confirmed';
+    const IN_PROGRESS = 'in progress';
+    const CLOSED = 'closed';
 }
