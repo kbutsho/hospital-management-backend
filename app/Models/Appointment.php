0c7f14076dfbcd681fb63259ee4bc35a1ecd7c27
Kaushik Biswas <kbutsho@gmail.com>
2024-01-23 15:45:47 +0600

    added schedule_id date to appointment

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Appointment extends Model
 {
-    use HasFactory, SoftDeletes;
-    protected $fillable = ['serial_id', 'serial_number', 'patient_id'];
+    use HasFactory;
+    protected $fillable = ['serial_id', 'schedule_id', 'serial_number', 'patient_id', 'date'];
 }
