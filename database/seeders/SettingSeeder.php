ea2b65ae35c898dfe518c327e8e958b6cbbaf5ae
Kaushik Biswas <kbutsho@gmail.com>
2024-04-20 20:35:19 +0600

    organization_name seeder update

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -14,7 +14,7 @@ class SettingSeeder extends Seeder
     {
         $settings = [
             [
-                'organization_name' => 'Apollo Hospital',
+                'organization_name' => 'Elite Care Hospital',
                 'address' => 'Greams Lane, 21, Greams Rd, Thousand Lights, Chennai, Tamil Nadu 600006, India',
                 'about' => "Welcome to our hospital management website, your comprehensive gateway to accessing vital information about our healthcare services. Our website is designed to provide patients and visitors with easy access to essential details, including department information and profiles of experienced doctors. Explore our various departments, each dedicated to specialized areas of medical care such as internal medicine, surgery, obstetrics and gynecology, pediatrics, emergency medicine, and radiology. Within each department, you'll find a team of skilled professionals committed to delivering compassionate and effective treatment. Our website also features profiles of our experienced doctors, showcasing their expertise and dedication to providing top-quality care to our patients. Whether you're seeking information about our services, looking to schedule an appointment, or simply want to learn more about our hospital, our website is here to guide you every step of the way.",
                 'phone' => '+8801749555864',
