c8563c249f79cf46a6e16cebef9cba5a26843b8e
Kaushik Biswas <kbutsho@gmail.com>
2024-05-06 05:46:32 +0600

    sslcommerze added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,15 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=   , initial-scale=1.0">
-    <title>api test</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>welcome</title>
 </head>
 
 <body>
-
+    <div style="height: 95vh; display:flex; justify-content: center; align-items: center">
+        <h3 style="text-transform: uppercase; font-wight: bold">welcome</h3>
+    </div>
 </body>
 
 </html>
